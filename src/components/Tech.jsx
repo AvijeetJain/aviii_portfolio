24f11cc70fce080e4e992b0f6d75715b100142e3
AvijeetJain <jainavijeet@gmail.com>
2023-04-10 03:55:51 +0530

    added tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "./hoc";
+import { technologies } from "../constants";
+
+import { motion } from "framer-motion";
+import { textVariant } from '../utils/motion';
+
+import { styles } from '../styles';
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <>
+
+    <motion.div variants={textVariant()}>
+      <p className={`${styles.sectionSubText} text-center`}>
+        Here are some of the technologies that i know
+      </p>
+      
+      <h2 className={`${styles.sectionHeadText} text-center`}>
+        Technologies
+      </h2>
+    </motion.div>
+
+    <div className="flex flex-row flex-wrap justify-center gap-10 mt-10">
+      {technologies.map((technology) => (
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} /> 
+        </div>
+      ))}
+    </div>
+
+    </>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "");
